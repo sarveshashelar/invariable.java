b50a15804d1c6f62cea621e6458ba4ada10d87ed
sarveshashelar <70604593+sarveshashelar@users.noreply.github.com>
2020-09-02 21:18:13 +0530

    Add files via upload

---
+++ instance.java
@@ -0,0 +1,15 @@
+public class instance_variable {
+	int year;
+	int month;
+	int date;                                                                                                                                               
+	public static void main(String[] args) {
+    obj.year = 2020;
+	obj.month = 8;
+	obj.date = 18;
+	System.out.println("Display date of first object"+ obj.date + "/" + obj.data + "/" + obj.month + "/" + obj.year)
+	instance_variable obj1 = new instance_variables();
+	System.out.println("Display data of second object of default value," + obj1.date + obj1.month + obj1.year);
+	
+	}
+
+}
